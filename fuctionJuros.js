c4cff050cd979c2b19aa6fc3f41174639f7b12b6
daniel-s-souza <danielsantsouza94@gmail.com>
2022-10-20 15:22:07 -0300

    fix: Refatora nome da função que calcula disconto ou juros

--- fuctionJuros.js
+++ fuctionJuros.js
@@ -1,6 +1,6 @@
 
-function getDiscount (valor, desconto) {
-  return (valor * (desconto/100));
+function aplyDiscountOrFees (valor, taxa) {
+  return (valor * (taxa/100));
 }
 
 function main () {
@@ -8,16 +8,16 @@ function main () {
   const condicaoPagamento = 3;
   // 1 = Débito, 2 = Dinheiro ou Pix, 3= 2x Credito, 4= Mais q 2x//
   if (condicaoPagamento === 1) {
-    const precoTotal = preco - getDiscount(preco, 10);
+    const precoTotal = preco - aplyDiscountOrFees(preco, 10);
     console.log(precoTotal);
   } else if (condicaoPagamento === 2) {
-    const precoTotal = preco - getDiscount(preco, 15);
+    const precoTotal = preco - aplyDiscountOrFees(preco, 15);
     console.log(precoTotal);
   } else if (condicaoPagamento === 3) {
     const precoTotal = preco
     console.log(precoTotal);
   } else {
-    const precoTotal = preco + getDiscount(preco, 10);
+    const precoTotal = preco + aplyDiscountOrFees(preco, 10);
     console.log(precoTotal);
   };
 };

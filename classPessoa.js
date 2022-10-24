b4b108511af6bd39329965d5d0c1e25594a4f57d
daniel-s-souza <danielsantsouza94@gmail.com>
2022-10-24 14:40:44 -0300

    feat: implementa logica para informar classificação do imc

--- classPessoa.js
+++ classPessoa.js
@@ -12,7 +12,20 @@ class Person {
   calcImc() {
     return this.weight / (this.height * this.height);
   }
+
+  ImcTable() {
+    if (this.calcImc() < 18.5 ){
+      return "Abaixo do Peso"
+    } else if (this.calcImc() >= 18.5 && this.calcImc() < 25) {
+      return "Peso Normal"
+    } else if (this.calcImc() >= 25 && this.calcIm() < 30) {
+      return "Sobrepeso"
+    } else {
+      return 'Obesidade'
+    }
+  }
 }
 
 const person = new Person('José', 1.75, 70);
 console.log(person.calcImc());
+console.log(person.ImcTable());
